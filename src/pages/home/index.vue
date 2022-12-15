64f963279adab2437e81a20235ad4e1a41c774bb
JieS-Lei <1693256432@qq.com>
2022-12-15 10:16:31 +0800

    删除无效代码

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
-        this is home
+    <div class="commin">
+        <el-container>
+            <el-header>Header</el-header>
+            <el-container>
+                <el-aside width="200px">Aside</el-aside>
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
     </div>
 </template>
 <script setup>
